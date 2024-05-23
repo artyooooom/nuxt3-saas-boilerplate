afeb0a4c2bdeeffd8da07b02929e0061d9bae247
artyom <agasparyan0000@gmail.com>
2024-05-23 12:30:05 +0400

    init shadcn-vue, nuxt & tailwindcss

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/Button'
+</script>
+
+<template>
+
+  <Button variant="destructive">
+    <span>Hello World</span>
+    <Icon name="material-symbols:chevron-right-rounded" size="1.5em"/>
+  </Button>
+  
+</template>
