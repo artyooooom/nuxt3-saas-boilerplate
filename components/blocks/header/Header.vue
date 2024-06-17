41a3cae3bd4e583f49053adf578a990959128e5d
artyom <agasparyan0000@gmail.com>
2024-06-17 16:11:26 +0400

    linking on header fixed

--- components/blocks/header/Header.vue
+++ components/blocks/header/Header.vue
@@ -13,7 +13,7 @@
       <header class="fixed w-full">
         <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
             <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
-                <a href="#" class="flex items-center">
+                <a href="/" class="flex items-center">
                     <img v-if="appLogo" :src="appLogo" :alt="appName" class="h-6 mr-3 sm:h-9" />
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ appName }}</span>
                 </a>
@@ -22,7 +22,7 @@
                         <NavigationMenu>
                             <NavigationMenuList>
                                 <NavigationMenuItem v-for="link in links" :key="link.label">
-                                    <NavigationMenuLink href="/" :class="navigationMenuTriggerStyle()">
+                                    <NavigationMenuLink :href="link.url" :class="navigationMenuTriggerStyle()">
                                     {{ link.label }}
                                     </NavigationMenuLink>
                                 </NavigationMenuItem>
