9292dd3f6a22926526abee4489c8470ec3ed26e6
artyom <agasparyan0000@gmail.com>
2024-06-22 21:15:46 +0400

    privacy policy & terms conditions static pages with prompts

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+definePageMeta({
+    layout: 'default'
+});
+</script>
+
+<template>
+    <!-- chat-gpt prompt for terms & conditions generation -->
+    <!-- My website name is [WEBSITE]
+        My business name is [BUSINESS NAME]
+        I sell [PRODUCT] for [NICHE] My email is [EMAIL]
+        My address is [Adress]. Today's date is [DATE]. -->
+    <div class="max-w-screen-xl mx-auto pt-32 pb-16 md:block px-4">
+        <!-- then insert it here -->
+    </div>
+</template>
+
