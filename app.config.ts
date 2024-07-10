0dfc4cb475533ef94dce57d0a49ade6b1b5415fa
artyom <agasparyan0000@gmail.com>
2024-07-10 15:28:06 +0400

    tawk.to -> tidio

--- app.config.ts
+++ app.config.ts
@@ -3,18 +3,6 @@ export default defineAppConfig({
     appLogo: null,
     supportChat: {
         isEnabled: true,
-        embed: `<!--Start of Tawk.to Script-->
-        <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/668b9513e1e4f70f24eeab40/1i28jsl1o';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();
-        </script>
-        <!--End of Tawk.to Script-->`
+        embed: `<script src="//code.tidio.co/jticdb77r6jy6wtt28ysaj4ahbif7iky.js" async></script>`
     }
 });
