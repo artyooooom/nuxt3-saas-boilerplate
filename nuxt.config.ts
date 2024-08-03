179ab8d4c9930cc8e4d7427d6bf5587ddad0da35
artyom <agasparyan0000@gmail.com>
2024-08-03 15:36:54 +0400

    smooth-scroll

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,11 @@ export default defineNuxtConfig({
       script: [{ src: 'https://unpkg.com/flowbite@1.4.1/dist/flowbite.js' }],
     },
   },
+  router: {
+    options: {
+      scrollBehaviorType: 'smooth'
+    }
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "shadcn-nuxt",
