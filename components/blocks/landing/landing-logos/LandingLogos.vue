09bdcc174b03138cebf0bde5a8a78a450043cbca
artyom <agasparyan0000@gmail.com>
2024-08-03 11:26:10 +0400

    landing logos on hover are colored now

--- components/blocks/landing/landing-logos/LandingLogos.vue
+++ components/blocks/landing/landing-logos/LandingLogos.vue
@@ -15,13 +15,13 @@ const gridColsClass = computed(() => {
 
     <section>
         <div class="max-w-screen-xl p-8 mx-auto">
-            <div class="grid gap-8 text-gray-500 dark:text-gray-400 max-sm:gap-14 max-sm:grid-cols-1"
+            <div class="grid text-gray-500 max-sm:gap-14 max-sm:grid-cols-1"
                 :class="gridColsClass">
 
                 <a v-for="logo in logos" :key="logo.alt" :href="logo.link" target="_blank"
                     class="flex items-center justify-self-center">
                     <div v-if="logo.type === 'svg'" v-html="logo.data"></div>
-                    <img v-else :src="logo.data" :alt="logo.alt">
+                    <img v-else :src="logo.data" class="max-h-16 saturate-0 hover:saturate-100 duration-150" :alt="logo.alt">
                 </a>
 
             </div>
