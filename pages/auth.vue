a5054cff282458dbc00edb79857d4ac331e02456
artyom <agasparyan0000@gmail.com>
2024-08-06 17:55:12 +0400

    fixed links on terms&conditions and privacy policy

--- pages/auth.vue
+++ pages/auth.vue
@@ -89,11 +89,11 @@ let handleFormRender = computed(() => {
 
         <p class="px-8 text-center text-sm text-muted-foreground">
           By clicking continue, you agree to our
-          <a href="/terms" class="underline underline-offset-4 hover:text-primary">
+          <a href="/terms-and-conditions" target="_blank" class="underline underline-offset-4 hover:text-primary">
             Terms of Service
           </a>
           and
-          <a href="/privacy" class="underline underline-offset-4 hover:text-primary">
+          <a href="/privacy-policy" target="_blank" class="underline underline-offset-4 hover:text-primary">
             Privacy Policy
           </a>
         </p>
