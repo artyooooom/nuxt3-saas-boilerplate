1d99c28c041ef8e93ce6e0afb70ed9a3e94a8d27
artyom <agasparyan0000@gmail.com>
2024-08-27 21:59:56 +0400

    updated landing

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
 
 import LandingHero from '@/components/blocks/landing/landing-hero/LandingHero.vue';
-import LandingSection from '@/components/blocks/landing/landing-section/LandingSection.vue';
-import LandingTestimonials from '@/components/blocks/landing/landing-testimonials/LandingTestimonials.vue';
-import LandingQuote from '@/components/blocks/landing/landing-quote/LandingQuote.vue';
-import LandingFAQ from '@/components/blocks/landing/landing-faq/LandingFAQ.vue';
-import LandingCTA from '@/components/blocks/landing/landing-cta/LandingCTA.vue';
-import LandingPricings from '@/components/blocks/landing/landing-pricings/LandingPricings.vue';
-import { type ButtonProps, type TestimonialProps, type PricingPlanProps, type AccordionItemProps } from '@/components/blocks/types';
+import { type ButtonProps } from '@/components/blocks/types';
 
 definePageMeta({
     layout: 'default'
@@ -17,21 +11,24 @@ const heroButtons: ButtonProps[] = [{
     title: 'Documentation',
     size: 'lg',
     link: {
-        url: '#pricings'
+        url: 'https://docs.nuxtsaas.tech/',
+        target: '_blank',
     }
 },
 {
     title: 'Authorization Page',
     size: 'lg',
     link: {
-        url: '/auth',
+        url: 'https://nuxtsaas.tech/auth',
+        target: '_blank',
     },
     variant: 'secondary'
 }, {
     title: 'Blog',
     size: 'lg',
     link: {
-        url: '/blog',
+        url: 'https://nuxtsaas.tech/blog',
+        target: '_blank',
     },
     variant: 'secondary'
 }]
@@ -47,39 +44,6 @@ let heroImage = {
     url: '/images/github-dark.png'
 }
 
-const CTAButton: ButtonProps[] = [{
-    title: 'Get Started',
-    link: {
-        url: '#pricings'
-    }
-}]
-
-const pricingPlans: PricingPlanProps[] = [
-    {
-        heading:"Starter Package",
-        pricing: {
-            actual: "$39",
-        },
-        features: ['Example'],
-        period:"Lifetime",
-        paymentLink:"https://buy.stripe.com/test_7sI01YdJDbla3qU144",
-        action: {
-            title: 'Get Started'
-        }
-    },
-    {
-        heading:"Starter Package [Subscription]",
-        pricing: {
-            actual: "$39",
-        },
-        features: ['Example'],
-        period:"day",
-        paymentLink:"https://buy.stripe.com/test_dR62a620Vah6aTmbIJ",
-        action: {
-            title: 'Get Started'
-        }
-    },
-]
 
 </script>
 
@@ -89,8 +53,6 @@ const pricingPlans: PricingPlanProps[] = [
 
         <LandingHero :description="heroDescription" :image="heroImage" :heading="heroHeading" :buttons="heroButtons"
             align="center" />
-
-        <LandingPricings :pricing-plans="pricingPlans"/>  
 
     </div>
 
